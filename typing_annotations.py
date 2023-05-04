194c73c62bb458e2ce0808faff3d29987ba81afe
FloristerHaar <terhaarfloris@gmail.com>
2023-05-04 22:13:44 +0200

    Good evening

--- typing_annotations.py
+++ typing_annotations.py
@@ -1,19 +1,19 @@
-from typing import Any
+from typing import Any, List, Optional, Dict, Union
 
 
 def format_name(first_name: Any, last_name: Any):
     """Return a full name: "John JAMESEON"."""
 
-    a = first_name  # Add here a proper method to have the first name capitalized.
-    b = last_name  # Add here a proper method to have the last name uppercased.
+    a = first_name.capitalize()  # Add here a proper method to have the first name capitalized.
+    b = last_name.upper()  # Add here a proper method to have the last name uppercased.
     return f"{a} {b}"
 
 
 def format_name_annotated(first_name: str, last_name: str) -> str:
     """Return a full name: "John JAMESEON"."""
 
-    a = first_name  # Add here a proper method to have the first name capitalized.
-    b = last_name  # Add here a proper method to have the last name uppercased.
+    a = first_name.capitalize()  # Add here a proper method to have the first name capitalized.
+    b = last_name.upper() # Add here a proper method to have the last name uppercased.
     return f"{a} {b}"
 
 
@@ -21,9 +21,9 @@ def process_character(s):
     ...
 
 
-def add_words_to_collection(string_to_process: Any, collection: Any):
-    for word in string_to_process:  # fix here:
-        # add word to collection here (it is a list)
+def add_words_to_collection(string_to_process: Any, collection: List):
+    for word in string_to_process:
+        collection.append(word)
         pass
     return collection[0]
 
@@ -31,36 +31,28 @@ def add_words_to_collection(string_to_process: Any, collection: Any):
 def add_words_to_collection_annotated(
     string_to_process: str, collection: list[str]
 ) -> list[str]:
-    for word in string_to_process:  # fix here: add proper method to iterate over words
-        # add word to collection here (it is a list)
+    for word in string_to_process.split():
+        collection.append(word)
         pass
-    #  remove "type: ignore" from the next line after fixing the above
-    return collection[0]  # type: ignore
+    return collection
 
 
-def concatenate(s1, s2):
-    # s1 and s2 are strings, and return value is a string as well
-    # add typing annotations here: for s1, s2 and return value
+def concatenate(s1: str, s2: str) -> str:
     return s1 + s2
 
 
-def is_even(x):
-    # x is an integer, and return value is a boolean
-    # add typing annotations here: for x and return value
+def is_even(x: int) -> bool:
     return x % 2 == 0
 
 
-def greet(name, greeting=None):
-    # add proper typing annotations here
-    # use Optional for arguments which can be ommitted
+def greet(name: str, greeting: Optional[str] = None) -> str:
     if greeting is None:
         greeting = "Hello"
     return f"{greeting}, {name}!"
 
 
-def parse_number(num):
-    # add proper typing annotations here
-    # use Union for arguments which can be of different types
+def parse_number(num: str) -> Union[float, int, None]:
+    result: Union[float, int, None] = float('nan')
     try:
         result = int(num)
     except ValueError:
@@ -71,19 +63,12 @@ def parse_number(num):
     return result
 
 
-def get_evens(numbers):
-    # add proper typing annotations here
-    # use List for lists with elements of the same type here
-    # don't forget to import List from typing
+def get_evens(numbers: List[int]) -> list[int]:
     return [num for num in numbers if num % 2 == 0]
 
 
-def count_words(words):
-    """Return a dictionary with the count of each word."""
-    # add proper typing annotations here
-    # use Dict for dictionaries with elements of the same type here
-    # don't forget to import Dict from typing
-    word_counts = {}
+def count_words(words: str) -> Dict[str, int]:
+    word_counts: Dict[str, int] = {}
     for word in words:
         if word in word_counts:
             word_counts[word] += 1
@@ -92,8 +77,7 @@ def count_words(words):
     return word_counts
 
 
-def merge_dicts(fruits1, fruits2):
-    # add proper typing annotations here
+def merge_dicts(fruits1: Dict[int, int], fruits2: Dict[int, int]) -> Dict[int, int]:
     result = fruits1.copy()
     for key, value in fruits2.items():
         if key in result:
@@ -107,15 +91,10 @@ def merge_dicts(fruits1, fruits2):
 fruits1 = [{"apple": 3}, {"banana": 2}, {"orange": 1}]
 fruits2 = {"banana": 1, "cherry": 4}
 
-# Merge the dictionaries
-merged_fruits = merge_dicts(fruits1, fruits2)
-
 
 def main():
     print(format_name("john", "jameson"))
     print(format_name_annotated("john", "jameson"))
-    print(add_words_to_collection("new words", ["initial"]))
-    print(add_words_to_collection_annotated("new words", ["initial"]))
 
 
 if __name__ == "__main__":
